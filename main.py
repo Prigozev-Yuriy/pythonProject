f287dfd626eaf90b5bab838512cc8c8e0853034a
PrigojevYuV <summerqtyf@gmail.com>
2023-12-07 15:41:01 +0300

    merge commit

--- main.py
+++ main.py
@@ -31,4 +31,15 @@ if __name__ == '__main__':
     capitalize("python programming")  # [Python Programming]
     capitalize("how are you!")  # [How Are You!]
 
+
+    def merge(dic1, dic2):
+        dic3 = dic1.copy()
+        dic3.update(dic2)
+        return dic3
+
+
+    dic1 = {1: "hello", 2: "world"}
+    dic2 = {3: "Python", 4: "Programming"}
+    merge(dic1, dic2)  # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
